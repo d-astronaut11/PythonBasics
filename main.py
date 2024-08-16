8f04362ddf864b65c15e254c02d66e63ab4ddefc
d-astronaut11 <129549283+d-astronaut11@users.noreply.github.com>
2024-08-16 07:32:03 +0000

    task 8 - fixed

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 
 import folium
 import requests
+from dotenv import load_dotenv
 from flask import Flask
 from geopy import distance
 
-API = os.environ["API_YANDEX"]
+load_dotenv()
+API = os.getenv("API_YANDEX")
 FILE_NAME = "coffee.json"
-ENCONDING = "CP1251"
+ENCODING = "CP1251"
 
 
 def get_content_of_file(file_name, encoding):
@@ -20,15 +22,20 @@ def get_content_of_file(file_name, encoding):
 
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(
-        base_url,
-        params={
-            "geocode": address,
-            "apikey": apikey,
-            "format": "json",
-        },
-    )
-    response.raise_for_status()
+    try:
+        response = requests.get(
+            base_url,
+            params={
+                "geocode": address,
+                "apikey": apikey,
+                "format": "json",
+            },
+        )
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Ошибка при обращении к API Yandex: {e}")
+        return None
+
     found_places = response.json()["response"]["GeoObjectCollection"]["featureMember"]
 
     if not found_places:
@@ -54,8 +61,8 @@ def form_list_of_coffee_bars(bars, coords):
     return list_of_bars
 
 
-def get_distance_name(dict):
-    return dict["distance"]
+def get_distance_name(bar):
+    return bar["distance"]
 
 
 def get_bars_map(bars, coords):
@@ -78,23 +85,33 @@ def open_html_file():
         return file.read()
 
 
-def main():
-    file_contents = get_content_of_file(FILE_NAME, ENCONDING)
-    my_location_coords = fetch_coordinates(API, input("Где вы находитесь? "))
-    if my_location_coords is None:
-        print("Не удалось найти координаты для указанного местоположения.")
-    else:
-        print("Ваши координаты:", my_location_coords)
-        list_of_coffee_bars = form_list_of_coffee_bars(file_contents, my_location_coords)
-        nearest_coffees = sorted(list_of_coffee_bars, key=get_distance_name)[:5]
-        coords_for_map = (my_location_coords[1], my_location_coords[0])
-        map = get_bars_map(nearest_coffees, coords_for_map)
-        map.save("coffee_map.html")
-        print("Карта сохранена в файл 'coffee_map.html'")
+def create_map_with_nearest_coffees(api_key, file_name, encoding, location):
+    file_contents = get_content_of_file(file_name, encoding)
+    coords = fetch_coordinates(api_key, location)
 
-        app = Flask(__name__)
-        app.add_url_rule("/", "map", open_html_file)
-        app.run("0.0.0.0")
+    while coords is None:
+        location = input(
+            "Не удалось найти координаты для указанного местоположения. Пожалуйста, попробуйте снова: "
+        )
+        coords = fetch_coordinates(api_key, location)
+
+    list_of_coffee_bars = form_list_of_coffee_bars(file_contents, coords)
+    nearest_coffees = sorted(list_of_coffee_bars, key=get_distance_name)[:5]
+    coords_for_map = (coords[1], coords[0])
+    map = get_bars_map(nearest_coffees, coords_for_map)
+    map.save("coffee_map.html")
+
+
+def run_flask_app():
+    app = Flask(__name__)
+    app.add_url_rule("/", "map", open_html_file)
+    app.run("0.0.0.0")
+
+
+def main():
+    location = input("Где вы находитесь? ")
+    create_map_with_nearest_coffees(API, FILE_NAME, ENCODING, location)
+    run_flask_app()
 
 
 if __name__ == "__main__":
